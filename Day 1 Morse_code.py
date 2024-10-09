b9d26d22269245acc34f80874676ccea4baf5384
404 <102517230+sudo-404-hub@users.noreply.github.com>
2024-10-09 11:12:30 +0530

    Update Day 1 Morse_code.py

--- Day 1 Morse_code.py
+++ Day 1 Morse_code.py
@@ -8,55 +8,28 @@
 # commas, colons, apostrophes, periods, exclamation marks, and question marks. The function 
 # should return the Morse code equivalent for the string.
 
-morse_code_dict = {
-    "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.",
-    "G": "--.", "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..",
-    "M": "--", "N": "-.", "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.",
-    "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
-    "Y": "-.--", "Z": "--..", "(": "-.--.", ")": "-.--.-", ",": "--..--",
-    ".": ".-.-.-", "?": "..--..", "'": ".----.", ":": "---...", ";": "-.-.-.",
-    "-": "-....-", "_": "..--.-", "/": "-..-.", "!": "-.-.--", "$": "...-..-",
-    "&": ".-...", "+": ".-.-.", "=": "-...-", "@": ".--.-.", "¡": "--...-",
-    "¿": "..-.-", "\"": ".-..-."
-}
+
+morse_code_dict = {"A": ".-","B": "-...","C": "-.-.","D": "-..","E": ".","F": "..-.","G": "--.","H": "....","I": "..","J": ".---","K": "-.-","L": ".-..","M": "--","N": "-.","O": "---","P": ".--.","Q": "--.-","R": ".-.","S": "...","T": "-","U": "..-","V": "...-","W": ".--","X": "-..-","Y": "-.--","Z": "--..","(": "-.--.",")": "-.--.-",",": "--..--",".": ".-.-.-","?": "..--..","'": ".----.",":": "---...",";": "-.-.-.","-": "-....-","_": "..--.-","/": "-..-.","!": "-.-.--","$": "...-..-","&": ".-...","+": ".-.-.","=": "-...-","@": ".--.-.","¡": "--...-","¿": "..-.-","\"": ".-..-."}
 
 def encode(string):
-    encoded_message = []
-    for char in string:
-        if char in morse_code_dict:
-            encoded_message.append(morse_code_dict[char])
-        else:
-            encoded_message.append('')  # Append empty string for unsupported characters
-    print(' '.join(encoded_message))
+    for me in string : # loop start
+        print(morse_code_dict[me] , end=" ")
+
 
 def decode(string):
-    morse_to_char = {value: key for key, value in morse_code_dict.items()}
-    decoded_message = []
-    morse_words = string.split(' ')  # Split the input Morse code by spaces
-    for morse_char in morse_words:
-        if morse_char in morse_to_char:
-            decoded_message.append(morse_to_char[morse_char])
-        else:
-            decoded_message.append('?')  # Append '?' for unsupported Morse code
-    print(''.join(decoded_message))
+    reversed_morse = {value: key for key, value in morse_code_dict.items()} # change the key to value and value to key of morse_code_dict
+    newstring = string.split() # break the string where from where is space
+    for me in newstring:
+        print(reversed_morse[me], end="")
 
 while True:
-    maininput = input("Enter 1 to encode \nEnter 2 to decode \nEnter q to exit :: ")
+    maininput = input("Enter 1 to encode \n enter 2 to decode \n enter q to exit :: ")
     if maininput == "1":
-        encode(input("Enter your message to encode: ").upper())
-    elif maininput == "2":
-        decode(input("Enter your encrypted key to decode it: "))
-    elif maininput == "q":
+        encode(input("Enter your message to encode : ").upper())
+    if maininput == "2":
+        decode(input("Enter you encryted key to decode it : "))
+    if maininput == "q":
         print("BYE!!")
-        break
-    else:
-        print("Invalid input. Please try again.")
-
-
-    
-
-
-
 
 
 
