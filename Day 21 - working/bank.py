6308f91b7bc417867add4dc7dd825f8142c4352e
404 <102517230+sudo-404-hub@users.noreply.github.com>
2024-12-01 22:59:00 +0530

    Update bank.py

--- Day 21 - working/bank.py
+++ Day 21 - working/bank.py
@@ -1,38 +1,74 @@
 # ask: Create a Simple Class for a BankAccount
-# Define a Class: Create a class named BankAccount.
-# Attributes: The class should have the following attributes:
-# account_holder (string): The name of the account holder.
-# balance (float): The current balance of the account (initialize it to 0.0).
-# account_number (string): A unique account number for the bank account.
-# Methods: The class should have the following methods:
-# deposit(amount): This method should add the specified amount to the balance and print a confirmation message.
-# withdraw(amount): This method should subtract the specified amount from the balance if sufficient funds are available; otherwise, it should print an error message.
-# get_balance(): This method should return the current balance of the account.
-# transfer(amount, other_account): This method should transfer the specified amount to another BankAccount instance if sufficient funds are available; otherwise, it should print an error message.
-# Create Instances: Create at least two instances of the BankAccount class, perform some deposits and withdrawals, and demonstrate transferring money between accounts.
 
-# later on feature can be add
-# user input interaction
-# new account create , del 
+print("Welcome to MOST WANTED BANK")
+
 
 class BankAccount:
-    def __init__(self, account_holder, account_number):
-        self.account_holder = account_holder
-        self.balance = ""
-        self.account_number = account_number
+    def __init__(self):
+        self.account_holder = ["admin"]
+        self.balance = 0
+        self.account_number = ["123"]
+        
+    def help(self):
+        print("Enter [add] To Add money: \n Enter [withdraw] to withdraw the money \n Enter [Balance] to see ur bank balance :\n")
 
     def deposit(self, amount):
-        if self.account_number == self.account_holder:
-            self.balance.append(amount)
+            self.balance = self.balance + int(amount)
             print(f"Your back balance is {self.balance}")
 
     def withdraw(self, amount):
-        if self.account_number == self.account_holder and amount in self.balance:
-            self.balance - amount
+        self.balance = self.balance - int(amount)
+        print(f"Your back balance is {self.balance}")
+            
 
 
+    def login_page(self):
+        
+        while True:
+            account_holder_name_input = input("Enter you account_holder name : ")
+            if account_holder_name_input not in self.account_holder:
+                print("Account holder name not fount in database.")
+            else:
+                account_number_input = input("Enter ur account nunber : ")
+                if account_number_input not in self.account_number:
+                    print("U enter wrong account number .")
+                else:
+                    print(f"welcome back {self.account_holder} to MOST WANTED Bank.")
+                    mybank.loged_in()
+            break
 
 
+                        
+    
+    def loged_in(self):
+        self.help()
+        while True:
+            main_input = input("Enter : ")
+
+            if main_input == "add":
+                add_amount = input("Enter the amount to add to ur bank : ")
+                mybank.deposit(add_amount)
+                print(f"\n{self.account_holder} adding {add_amount} to ur Bank account\n")
+
+            if main_input == "withdraw":
+                print("withdrawing")
+
+            if main_input == "balance":
+                print(f"\n {self.balance} Your balance is ")
+
+            else:
+                print("\n-----OOPS-----You enter wrong command --------\n")
+                self.help()
+            
+ 
+
+mybank = BankAccount()
+mybank.login_page()
+
+# reaming feature ----------------------------
+# create new BankAccount
+# del BankAccount
+# validation if user have infugh many to withdraw from account
 
 
 
